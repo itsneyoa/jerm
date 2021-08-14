163d775151db417e8db46f7d91f462d9d4aa7b88
neyoa <itstomsmart@gmail.com>
2021-08-14 10:11:44 +0100

    Make site lmao

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
-import 'tailwindcss/tailwind.css'
+import Head from 'next/head'
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover" />
+        <meta name="description" content="Jay perm is a very bald non" />
+        <meta name="keywords" content="Jperm, Jperrm, Bald, Non" />
+        <title>Jperm is a bald non</title>
+
+        <link rel="manifest" href="/manifest.json" />
+
+        {/* <link href="/icons/16x16.png" rel="icon" type="image/png" sizes="16x16" />
+        <link href="/icons/32x32.png" rel="icon" type="image/png" sizes="32x32" />
+        <link rel="apple-touch-icon" href="/icons/apple-touch.png" /> */}
+
+        <meta name="theme-color" content="#f42069" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
